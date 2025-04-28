f62e79b5cd2fc43e50833bc8cee2d85b5edcc689
Corey <755349+corvec@users.noreply.github.com>
2025-04-28 15:27:02 -0400

    UI updates to make this compatible with Kokoro. Remove concurrency_limit as it was throwing an error.

--- app.py
+++ app.py
@@ -28,21 +28,92 @@ def tts(text, model, voice, api_key, base_url=None):
 
     return temp_file.name
 
+openai_voices = ['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer']
+kokoro_voices = [
+    "af_alloy",
+    "af_aoede",
+    "af_bella",
+    "af_heart",
+    "af_jadzia",
+    "af_jessica",
+    "af_kore",
+    "af_nicole",
+    "af_nova",
+    "af_river",
+    "af_sarah",
+    "af_sky",
+    "af_v0",
+    "af_v0bella",
+    "af_v0irulan",
+    "af_v0nicole",
+    "af_v0sarah",
+    "af_v0sky",
+    "am_adam",
+    "am_echo",
+    "am_eric",
+    "am_fenrir",
+    "am_liam",
+    "am_michael",
+    "am_onyx",
+    "am_puck",
+    "am_santa",
+    "am_v0adam",
+    "am_v0gurney",
+    "am_v0michael",
+    "bf_alice",
+    "bf_emma",
+    "bf_lily",
+    "bf_v0emma",
+    "bf_v0isabella",
+    "bm_daniel",
+    "bm_fable",
+    "bm_george",
+    "bm_lewis",
+    "bm_v0george",
+    "bm_v0lewis",
+    "ef_dora",
+    "em_alex",
+    "em_santa",
+    "ff_siwis",
+    "hf_alpha",
+    "hf_beta",
+    "hm_omega",
+    "hm_psi",
+    "if_sara",
+    "im_nicola",
+    "jf_alpha",
+    "jf_gongitsune",
+    "jf_nezumi",
+    "jf_tebukuro",
+    "jm_kumo",
+    "pf_dora",
+    "pm_alex",
+    "pm_santa",
+    "zf_xiaobei",
+    "zf_xiaoni",
+    "zf_xiaoxiao",
+    "zf_xiaoyi",
+    "zm_yunjian",
+    "zm_yunxi",
+    "zm_yunxia",
+    "zm_yunyang"
+]
+
 def gradio_interface():
     with gr.Blocks() as demo:
-        gr.Markdown("# <center> OpenAI Text-To-Speech </center>")
+        gr.Markdown("# <center> Kokoro FastAPI Text-To-Speech </center>")
         with gr.Row(variant='panel'):
-            api_key = gr.Textbox(type='password', label='OpenAI API Key', placeholder='Enter your OpenAI API key')
-            base_url = gr.Textbox(label='API Base URL', placeholder='http://localhost:8880/v1', value='')
+            api_key = gr.Textbox(type='password', label='API Key', placeholder='Enter your OpenAI API key', value='not-needed')
+            base_url = gr.Textbox(label='API Base URL', placeholder='http://localhost:8880/v1', value='http://localhost:8880/v1')
             model = gr.Dropdown(choices=['tts-1','tts-1-hd'], label='Model', value='tts-1')
-            voice = gr.Dropdown(choices=['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer'], label='Voice Options', value='alloy')
+            voice = gr.Dropdown(choices=kokoro_voices, label='Voice Options', value=kokoro_voices[0])
 
         text = gr.Textbox(label="Input text", placeholder="Enter your text and then click on the 'Generate' button, or press the Enter key.")
         btn = gr.Button("Generate")
         output_audio = gr.Audio(label="Speech Output")
 
-        text.submit(fn=tts, inputs=[text, model, voice, api_key, base_url], outputs=output_audio, api_name="tts_enter_key", concurrency_limit=None)
-        btn.click(fn=tts, inputs=[text, model, voice, api_key, base_url], outputs=output_audio, api_name="tts_button", concurrency_limit=None)
+        text.submit(fn=tts, inputs=[text, model, voice, api_key, base_url], outputs=output_audio, api_name="tts_enter_key")
+        btn.click(fn=tts, inputs=[text, model, voice, api_key, base_url], outputs=output_audio, api_name="tts_button")
 
     demo.launch()
 
